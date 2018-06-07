f90b30e02fff28fed5a5fbca1d56c2b813ccf5a6
Mai <maiasalguero@gmail.com>
2018-06-07 16:45:18 +0200

    firebase

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+export const DB_CONFIG = {apiKey: "AIzaSyC1BJCdR56e3SpJQR8pQ3DEn32yTphnbOw",
+    authDomain: "notesreact-7c2b0.firebaseapp.com",
+    databaseURL: "https://notesreact-7c2b0.firebaseio.com",
+    projectId: "notesreact-7c2b0",
+    storageBucket: "",
+    messagingSenderId: "1010029808033"}
